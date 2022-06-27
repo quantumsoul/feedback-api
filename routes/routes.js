153cfc0cd3d269db4a72f92c0d67caed56f3896f
aviwer <prakhar24012000@gmail.com>
2022-06-27 17:02:05 +0530

    init commit

--- routes/routes.js
+++ routes/routes.js
@@ -3,3 +3,4 @@ const Controller = require("./../controllers/controller");
 const router = express.Router();
 router.post("/order/feedback",Controller.giveFeedback);
 router.get("/order/get/feedback",Controller.getFeedback);
+module.exports = router
